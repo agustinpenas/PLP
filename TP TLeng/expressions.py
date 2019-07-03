874d3e41fe2e18598340d51bfbd0648dfd143461
Sorongoku <gonzalorpg@gmail.com>
2019-07-03 16:44:27 -0300

    Agregue la funcion para chequear referencias circ
    con comentarios

--- TP TLeng/expressions.py
+++ TP TLeng/expressions.py
@@ -8,8 +8,30 @@ class PrimaryVarDeclaration():
 	def evaluate(self):
 		field.setDicc(self.dicc)
 		var_next.setDicc(self.dicc)
+		self.refCheck(self.dicc)
 		exp = field.generate()
 		return exp
+
+	def refCheck(self, dicc):
+		diccKeys = list(dicc.keys()) #creo una lista nueva con las claves del diccionario
+		while (diccKeys): #mientras siga habiendo claves por revisar
+			p = diccKeys[0] #agarro una clave arbitraria, la primera
+			diccKeys.remove(p) #la saco de las claves porque ya la voy a revisar
+			camino = list(p) #empiezo el camino de las referencias con p siendo el punto de partida
+			self.DFS(p,diccKeys,dicc,camino)
+
+	def DFS(self,p,diccKeys,dicc,camino):
+		k= dicc.get(p) #accedo a la estructura del E correspondiende (sigo la referencia)
+		while (k.referencias): #mientras siga habiendo referencias sin revisar
+			c = k.referencias[0] #tomo referencia arbitraria
+			if (c in camino): #si c ya está en el camino quiere decir que se formaría un ciclo
+				#error de referencia circular
+			else:
+				camino.append(c) #agrego c al camino
+				claves.remove(c) #lo saco de las claves porque como estoy por revisarlo ahora no hace falta que lo revise nuevamente
+				self.DFS(c,diccKeys,dicc,camino)
+				k.referencias.remove(c) #luego de chequear esta referencia la borro y continúo para no revisarla nuevamente
+		camino.remove(k) #si ya chequeé todos los caminos que se desprenden de k entonces debo pasar a chequear otros caminos
 
 class VarDeclaration():
 
